71728ffd3dd2e96cc52952c91896290d339127f7
nikolaskokhno <kokhno.mm@gmail.com>
2020-08-25 18:18:26 +0300

    Create footer componet / add OpenSans fonts

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            © abz.agency specially for the test task
+        </div>
+    )
+}
+
+export default Footer;
